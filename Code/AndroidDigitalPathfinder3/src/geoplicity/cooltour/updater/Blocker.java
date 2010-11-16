d6d9aafbb1577b1ca9d47ce8bcf6e94e13e6842e
b.j.drew <b.j.drew@1d335613-667f-75ff-c23c-0b96f91a0ca3>
2010-11-16 02:54:25 +0000

    Blocker Code

---
+++ Code/AndroidDigitalPathfinder3/src/geoplicity/cooltour/updater/Blocker.java
@@ -0,0 +1,60 @@
+package geoplicity.cooltour.updater;
+import java.io.*;
+
+public class Blocker {
+
+	/**
+	 * args[0] is the name of the file (eg. Olana)
+	 * args[1] is the size of the blocks (eg. 5000000)
+	 * args[2] is the location of the file (eg. C:\Test\server\sites\);
+	 */
+
+	public static void block(String name, String blockSize, String location) {
+		try {
+			FileInputStream fis = new FileInputStream(location + name + ".zip");
+			int size = Integer.parseInt(blockSize);
+			byte buffer[] = new byte[size];
+
+			int i = 1;
+			while (true) {
+				int bytesToTransfer = fis.read(buffer, 0, size);
+				if (bytesToTransfer == -1) //if none left
+					break;
+
+				String filename = location + name + i;
+				FileOutputStream fos = new FileOutputStream(filename);
+				fos.write(buffer, 0, bytesToTransfer);
+				fos.flush();
+				fos.close();
+
+				i++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * args[0] is the name of the file (eg. Olana)
+	 * args[1] is the number of blocks (eg. 6)
+	 * args[2] is the location of the file (eg. C:\Test\sdcard\geoplicity\);
+	 */
+	
+	public static void unblock(String name, String amount, String location){
+		try {
+			int numberOfBlocks = Integer.parseInt(amount);
+			FileOutputStream fos = new FileOutputStream(location+name+"\\temp\\"+name+".zip");
+			for(int i = 1;i<=numberOfBlocks;i++){
+				File f = new File(location+name+"\\temp\\"+name+i);
+				long sizeOfFile = f.length();
+				FileInputStream fis = new FileInputStream(f);
+				byte buffer[] = new byte[(int)sizeOfFile];
+				fis.read(buffer);
+				fos.write(buffer);
+				fos.flush();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Code/AndroidDigitalPathfinder3/src/geoplicity/cooltour/updater/UpdaterTester.java
@@ -0,0 +1,28 @@
+package geoplicity.cooltour.updater;
+public class UpdaterTester {
+
+	/**
+	 * args[0] is the location of the file on the server (eg. http://reachforthestarsmcc.com/sdodge/files/sites/)
+	 * args[1] is the name of the file (eg. Olana)
+	 * args[2] is the block size (eg. 5000000)
+	 * args[3] is the number of blocks (eg. 6)
+	 * args[4] is the base location to store on device (eg. C:\Test\sdcard\geoplicity\)
+	 */
+	public static void main(String[] args) {
+		//Blocker.block("Olana", "5000000", "C:\\Test\\Other\\Attempt2\\");
+		//Downloader.download("http://reachforthestarsmcc.com/sdodge/files/sites/", "Olana", "6", "C:\\Test\\sdcard\\geoplicity\\");
+		//Unblocker.unblock("Olana", "6", "C:\\Test\\sdcard\\geoplicity\\");
+		//ZipUnpacker.unpack("C:\\Test\\sdcard\\geoplicity\\", "Olana");
+		
+		//Blocker.block(args[1], args[2], "server");
+		//Downloader.download(args[0], args[1], args[3], args[4]);
+		//Unblocker.unblock(args[1], args[3], args[4]);
+		//ZipUnpacker.unpack(args[4], args[1]);
+		//CleanUp.deleteTemp(args[4]+args[1]+"\\temp");
+	}
+	public static void testThread() {
+		SiteUpdateDetails update = new SiteUpdateDetails();
+		SiteUpdaterThread updateThread = new SiteUpdaterThread(update);
+	}
+
+}
