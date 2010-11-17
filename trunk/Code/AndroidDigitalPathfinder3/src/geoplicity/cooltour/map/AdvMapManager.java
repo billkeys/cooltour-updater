808f0299e9ab82e34f3374f511f017accbc76106
william.keys1 <william.keys1@1d335613-667f-75ff-c23c-0b96f91a0ca3>
2010-11-17 02:47:45 +0000

    Adding AdvMapManager which refactors the ABCD algorithim

---
+++ trunk/Code/AndroidDigitalPathfinder3/src/geoplicity/cooltour/map/AdvMapManager.java
@@ -0,0 +1,223 @@
+package geoplicity.cooltour.map;
+
+import geoplicity.cooltour.gps.LocationManager;
+
+import org.geoplicity.mobile.util.Logger;
+import org.geoplicity.mobile.util.Property;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.util.Log;
+
+public class AdvMapManager extends MapManager {
+        
+         public AdvMapManager(Context context, int w, int h) {
+                 super(context, w, h);
+         }
+         
+         /**
+          * Initialize the map
+          */
+         protected void initMap(){
+         	Logger.log(Logger.TRACE, "initMap()");
+         	
+         	//load the tile properties
+         	String rootDir = Property.getProperty("app.root.dir");
+         	String mapProperties = rootDir + "/" + Property.getProperty("current.site")+
+         		Property.getProperty("map.props");
+         	Log.v("initMap", "Map Properties = " + mapProperties);
+
+         	Property.loadProperties(mapProperties);
+         	Tile.dir = Property.storageDevice + rootDir + "/" + Property.getProperty("current.site")+ "/images/";
+         	Log.v("initMap", "Tile.dir = " + Tile.dir);
+         	
+         	Tile.prefix = Property.takeProperty("map.tile.prefix");
+         	Tile.type = Property.takeProperty("map.tile.type");
+         	Tile.width = Integer.parseInt(Property.takeProperty("map.tile.width"));
+         	Tile.height = Integer.parseInt(Property.takeProperty("map.tile.height"));
+         	
+         	mapWidth = Integer.parseInt(Property.takeProperty("map.image.width"));
+         	mapHeight = Integer.parseInt(Property.takeProperty("map.image.height"));
+         	
+         	//Position the map center over the default location 
+         	double lon = Double.parseDouble(Property.takeProperty("map.default.lon"));
+         	double lat = Double.parseDouble(Property.takeProperty("map.default.lat"));
+         	
+         	//Position the user at the default location
+         	curPosition = LocationManager.GetInstance().GetPosition(lon, lat);
+         	
+         	//Move cursor to default location
+         	locator.update(curPosition.getX(), curPosition.getY());
+
+         	updateView();
+         	updateTiles();
+
+         	try{
+         		Config newConfig = getConfig();
+         		Logger.log(Logger.TRACE, "new config is " + newConfig);
+         		
+             	if(newConfig != curConfig){
+             		pageInTiles(newConfig);	
+             		curConfig = newConfig;
+             		
+             	}
+         	}catch(Exception e){
+         		Logger.log(Logger.TRAP, "An error occurred while initializing map: " + e.toString());
+         	}
+         	
+         	//Get the tiled layer's upper left corner and render the layer
+             layerX = layer[UPPER_LEFT].col * Tile.width - viewX;
+             layerY = layer[UPPER_LEFT].row * Tile.height - viewY;
+         }
+         
+         /**
+          * Page in the A configuration:
+          * 10
+          * 00
+          */
+         protected void pageA(){
+         	mapImage = Bitmap.createBitmap(Tile.width, Tile.height, BITMAP_CONFIG);
+         	Canvas canvas = new Canvas(mapImage);
+         	
+         	int tx = this.getViewX() / Tile.width;
+     		int ty = this.getViewY() / Tile.height;
+
+     		int x = this.getViewX() - tx * Tile.width;
+     		int y = this.getViewY() - ty * Tile.height;
+
+     		int w = (tx + 1) * Tile.width - this.getViewX();
+     		int h = (ty + 1) * Tile.height - this.getViewY();
+     		
+         	// 1 0
+     		// 0 0
+     		java.lang.Runtime.getRuntime().gc();
+     		Bitmap bitmap = BitmapFactory.decodeFile(getTileName(UPPER_LEFT));
+     		bitmap = Bitmap.createBitmap(bitmap, x, y, w, h);
+     		
+     		canvas.drawBitmap(bitmap, x, y, null);
+         }
+         
+         /**
+          * Page in the B configuration
+          * 11
+          * 00
+          */
+         protected void pageB(){
+         	mapImage = Bitmap.createBitmap(Tile.width * 2, Tile.height, BITMAP_CONFIG);
+         	Canvas canvas = new Canvas(mapImage);
+         	
+         	int tx = this.getViewX() / Tile.width;
+     		int ty = this.getViewY() / Tile.height;
+
+     		int x = this.getViewX() - tx * Tile.width;
+     		int y = this.getViewY() - ty * Tile.height;
+
+     		int w = (tx + 1) * Tile.width - this.getViewX();
+     		int h = (ty + 1) * Tile.height - this.getViewY();
+     		
+         	// 1 0
+     		// 0 0
+     		java.lang.Runtime.getRuntime().gc();
+     		Bitmap bitmap = BitmapFactory.decodeFile(getTileName(UPPER_LEFT));
+     		bitmap = Bitmap.createBitmap(bitmap, x, y, w, h);
+     		
+     		canvas.drawBitmap(bitmap, x, y, null);
+     		
+     		// 0 1
+     		// 0 0
+     		int w1 = Tile.width-w;
+     		bitmap = BitmapFactory.decodeFile(getTileName(UPPER_RIGHT));
+     		bitmap = Bitmap.createBitmap(bitmap, 0, y, w1, h);
+
+     		canvas.drawBitmap(bitmap, x+w, y, null);
+         }
+
+         /**
+          * Page in the C configuration
+          * 10
+          * 10
+          */
+         protected void pageC(){
+         	mapImage = Bitmap.createBitmap(Tile.width*2, Tile.height * 2, BITMAP_CONFIG);
+         	Canvas canvas = new Canvas(mapImage);
+         	
+         	int tx = this.getViewX() / Tile.width;
+     		int ty = this.getViewY() / Tile.height;
+
+     		int x = this.getViewX() - tx * Tile.width;
+     		int y = this.getViewY() - ty * Tile.height;
+
+     		int w = (tx + 1) * Tile.width - this.getViewX();
+     		int h = (ty + 1) * Tile.height - this.getViewY();
+     		
+         	// 1 0
+     		// 0 0
+     		java.lang.Runtime.getRuntime().gc();
+     		Bitmap bitmap = BitmapFactory.decodeFile(getTileName(UPPER_LEFT));
+     		bitmap = Bitmap.createBitmap(bitmap, x, y, w, h);
+     		
+     		canvas.drawBitmap(bitmap, x, y, null);
+         	
+         	// 0 0
+     		// 1 0
+     		int h1 = Tile.height-h;
+     		bitmap = BitmapFactory.decodeFile(getTileName(LOWER_RIGHT));
+     		bitmap = Bitmap.createBitmap(bitmap, x, 0, w, h1);
+
+     		canvas.drawBitmap(bitmap, x, y+h, null);
+     		System.out.println("TEST");
+     		Log.v("initMap", "TEST");
+         }
+         
+         /**
+          * Page in the D configuration
+          * 11
+          * 11
+          */
+         protected void pageD() {
+     		mapImage = Bitmap.createBitmap(Tile.width*2, Tile.height*2, BITMAP_CONFIG);
+     		Canvas canvas = new Canvas(mapImage);
+
+     		int tx = this.getViewX() / Tile.width;
+     		int ty = this.getViewY() / Tile.height;
+
+     		int x = this.getViewX() - tx * Tile.width;
+     		int y = this.getViewY() - ty * Tile.height;
+
+     		int w = (tx + 1) * Tile.width - this.getViewX();
+     		int h = (ty + 1) * Tile.height - this.getViewY();
+     		
+     		// 1 0
+     		// 0 0
+     		java.lang.Runtime.getRuntime().gc();
+     		Bitmap bitmap = BitmapFactory.decodeFile(getTileName(0));
+     		bitmap = Bitmap.createBitmap(bitmap, x, y, w, h);
+     		
+     		canvas.drawBitmap(bitmap, x, y, null);
+     		
+     		// 0 1
+     		// 0 0
+     		int w1 = Tile.width-w;
+     		bitmap = BitmapFactory.decodeFile(getTileName(1));
+     		bitmap = Bitmap.createBitmap(bitmap, 0, y, w1, h);
+
+     		canvas.drawBitmap(bitmap, x+w, y, null);
+     		
+     		// 0 0
+     		// 1 0
+     		int h1 = Tile.height-h;
+     		bitmap = BitmapFactory.decodeFile(getTileName(2));
+     		bitmap = Bitmap.createBitmap(bitmap, x, 0, w, h1);
+
+     		canvas.drawBitmap(bitmap, x, y+h, null);
+     		
+     		// 0 0
+     		// 0 1
+     		bitmap = BitmapFactory.decodeFile(getTileName(3));
+     		bitmap = Bitmap.createBitmap(bitmap, 0, 0, w1, h1);
+
+     		canvas.drawBitmap(bitmap, x+w,y+h, null);
+     	}
+}
